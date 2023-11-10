7bc8fcf069c8bbf82d59c49156c8e3c383950813
emmanuel.rodriguez <emmanuel.rodriguez@inegi.org.mx>
2023-11-10 17:59:12 -0600

    ajax

--- js/funciones.js
+++ js/funciones.js
@@ -2,14 +2,15 @@ function mensaje(){
 	alert("Hola")
 }
 
+
 function api(){
-	let url =  "https://developers.google.com/maps/documentation/directions/overview?hl=es-419"
-	$.get(url,function(res){
-	console.log(res)
-})
-
-	   
-
-
-	
+  $.ajax({
+    url: "https://developers.google.com/maps/documentation/directions/overview?hl=es-419",
+    xhrFields: {
+       withCredentials: true
+    }
+ },j=>{
+   console.log(j)
+ }
+  )
 }
