e0beb9a95424cc70afa505d2ba11053fb3883b84
emmanuel.rodriguez <emmanuel.rodriguez@inegi.org.mx>
2023-11-09 20:40:27 -0600

    lee web serices

--- js/funciones.js
+++ js/funciones.js
@@ -3,5 +3,15 @@ function mensaje(){
 }
 
 function mensaje(){
-	alert("Api")
+	let url =  "https://www.chedraui.com.mx/_v/segment/graphql/v1?workspace=master&maxAge=short&appsEtag=remove&domain=store&locale=es-MX&operationName=productSearchV3&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2240b843ca1f7934d20d05d334916220a0c2cae3833d9f17bcb79cdd2185adceac%22%2C%22sender%22%3A%22vtex.store-resources%400.x%22%2C%22provider%22%3A%22vtex.search-graphql%400.x%22%7D%2C%22variables%22%3A%22eyJoaWRlVW5hdmFpbGFibGVJdGVtcyI6ZmFsc2UsInNrdXNGaWx0ZXIiOiJBTEwiLCJzaW11bGF0aW9uQmVoYXZpb3IiOiJkZWZhdWx0IiwiaW5zdGFsbG1lbnRDcml0ZXJpYSI6Ik1BWF9XSVRIT1VUX0lOVEVSRVNUIiwicHJvZHVjdE9yaWdpblZ0ZXgiOmZhbHNlLCJtYXAiOiJmdCIsInF1ZXJ5IjoieW9ndXJ0Iiwib3JkZXJCeSI6Ik9yZGVyQnlTY29yZURFU0MiLCJmcm9tIjowLCJ0byI6NDksInNlbGVjdGVkRmFjZXRzIjpbeyJrZXkiOiJmdCIsInZhbHVlIjoieW9ndXJ0In1dLCJmdWxsVGV4dCI6InlvZ3VydCIsImZhY2V0c0JlaGF2aW9yIjoiU3RhdGljIiwiY2F0ZWdvcnlUcmVlQmVoYXZpb3IiOiJkZWZhdWx0Iiwid2l0aEZhY2V0cyI6ZmFsc2UsInZhcmlhbnQiOiIifQ%3D%3D%22%7D"
+
+
+	$.get(url,function(res){
+	console.log(res)
+})
+
+	   
+
+
+	
 }
